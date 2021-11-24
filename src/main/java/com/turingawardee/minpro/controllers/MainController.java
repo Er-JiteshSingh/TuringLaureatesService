8e1eac3e492353c5ddd895142443e051029375b4
Geetesh Kumar <geeteshsahu6@gmail.com>
2021-11-24 23:17:36 +0530

    Bug Fixed & JPA Integration Completed

--- src/main/java/com/turingawardee/minpro/controllers/MainController.java
+++ src/main/java/com/turingawardee/minpro/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.turingawardee.minpro.controllers;
 
 import com.turingawardee.minpro.entities.AwardWinner;
 import com.turingawardee.minpro.services.AwardeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ import java.util.List;
 @RestController
 public class MainController {
 
+    @Autowired
+    private AwardeeService awardeeService;
+
     @GetMapping("/awardees")
     public List<AwardWinner> allAwardees() {
 
-        return new AwardeeService().getAllWinners();
+        return awardeeService.getAllWinners();
     }
 }
--- src/main/java/com/turingawardee/minpro/services/AwardeeService.java
+++ src/main/java/com/turingawardee/minpro/services/AwardeeService.java
@@ -3,9 +3,11 @@ package com.turingawardee.minpro.services;
 import com.turingawardee.minpro.dao.AwardeeRepository;
 import com.turingawardee.minpro.entities.AwardWinner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AwardeeService {
 
     @Autowired
